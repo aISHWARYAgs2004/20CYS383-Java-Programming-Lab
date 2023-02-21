e82ef1dde8c9dacee03ca082c2e70f8397f82f7a
Aishwarya GS <92919831+aISHWARYAgs2004@users.noreply.github.com>
2023-02-21 23:13:43 +0530

    Delete CODES directory

--- CODES/PYTHON/divisiblity.py
+++
@@ -1,6 +0,0 @@
-#checking if a given number is divisible by 6
-n=int(input("enter number: "))
-if n%2==0 and n%3==0:
-    print("number is divisible by 6")
-else:
-    print("number is not divisible by 6")
